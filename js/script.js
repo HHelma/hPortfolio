de8cf69b0ba8c462a321ff5e5cfc3cd3f4bcd72b
HHelma <hudaibailma@gmail.com>
2022-03-12 19:17:45 +0600

    portfolio done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Student","Freelancer","Graphic Designer","Businesswoman"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
